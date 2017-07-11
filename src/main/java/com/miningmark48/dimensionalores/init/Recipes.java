0c9079bf1779d421cccdfbb0271cde8a4aa36c89
MiningMark48 <miningmark48@gmail.com>
2017-07-10 22:46:15 -0400

    Recipes

--- src/main/java/com/miningmark48/dimensionalores/init/Recipes.java
+++ src/main/java/com/miningmark48/dimensionalores/init/Recipes.java
@@ -16,6 +16,16 @@ public class Recipes {
             GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_emerald), new ItemStack(Items.EMERALD), 1.25f);
             GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_redstone), new ItemStack(Items.REDSTONE), 1.25f);
             GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_lapis), new ItemStack(Items.DYE, 1, 4), 1.25f);
+
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_aluminium), new ItemStack(ModItems.ingot_aluminium), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_cobalt), new ItemStack(ModItems.ingot_cobalt), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_copper), new ItemStack(ModItems.ingot_copper), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_lead), new ItemStack(ModItems.ingot_lead), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_silver), new ItemStack(ModItems.ingot_silver), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_tin), new ItemStack(ModItems.ingot_tin), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_titanium), new ItemStack(ModItems.ingot_titanium), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_uranium), new ItemStack(ModItems.ingot_uranium), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_zinc), new ItemStack(ModItems.ingot_zinc), 1.25f);
         }
 
         if(ConfigurationHandler.add_end_ores) {
@@ -27,10 +37,30 @@ public class Recipes {
             GameRegistry.addSmelting(new ItemStack(ModBlocks.end_redstone), new ItemStack(Items.REDSTONE), 1.25f);
             GameRegistry.addSmelting(new ItemStack(ModBlocks.end_lapis), new ItemStack(Items.DYE, 1, 4), 1.25f);
             GameRegistry.addSmelting(new ItemStack(ModBlocks.end_quartz), new ItemStack(Items.DYE, 1, 4), 1.25f);
+
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_aluminium), new ItemStack(ModItems.ingot_aluminium), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_cobalt), new ItemStack(ModItems.ingot_cobalt), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_copper), new ItemStack(ModItems.ingot_copper), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_lead), new ItemStack(ModItems.ingot_lead), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_silver), new ItemStack(ModItems.ingot_silver), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_tin), new ItemStack(ModItems.ingot_tin), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_titanium), new ItemStack(ModItems.ingot_titanium), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_uranium), new ItemStack(ModItems.ingot_uranium), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_zinc), new ItemStack(ModItems.ingot_zinc), 1.25f);
         }
 
         if(ConfigurationHandler.add_overworld_ores){
             GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_quartz), new ItemStack(Items.QUARTZ), 1.25f);
+
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_aluminium), new ItemStack(ModItems.ingot_aluminium), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_cobalt), new ItemStack(ModItems.ingot_cobalt), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_copper), new ItemStack(ModItems.ingot_copper), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_lead), new ItemStack(ModItems.ingot_lead), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_silver), new ItemStack(ModItems.ingot_silver), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_tin), new ItemStack(ModItems.ingot_tin), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_titanium), new ItemStack(ModItems.ingot_titanium), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_uranium), new ItemStack(ModItems.ingot_uranium), 1.25f);
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_zinc), new ItemStack(ModItems.ingot_zinc), 1.25f);
         }
         
     }
