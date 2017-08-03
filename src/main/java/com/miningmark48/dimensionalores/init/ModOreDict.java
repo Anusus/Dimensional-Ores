092615270ead18c77327596624f882120cb170a7
MiningMark48 <miningmark48@gmail.com>
2017-08-02 23:28:57 -0400

    Fixed some bugs
    Closes #7 & #8

--- src/main/java/com/miningmark48/dimensionalores/init/ModOreDict.java
+++ src/main/java/com/miningmark48/dimensionalores/init/ModOreDict.java
@@ -1,68 +1,69 @@
 package com.miningmark48.dimensionalores.init;
 
+import com.miningmark48.dimensionalores.handler.ConfigurationHandler;
 import net.minecraftforge.oredict.OreDictionary;
 
 public class ModOreDict{
 
     public static void registerOreNames(){
 
-        OreDictionary.registerOre("oreCoal", ModBlocks.nether_coal);
-        OreDictionary.registerOre("oreCoal", ModBlocks.end_coal);
-        OreDictionary.registerOre("oreIron", ModBlocks.nether_iron);
-        OreDictionary.registerOre("oreIron", ModBlocks.end_iron);
-        OreDictionary.registerOre("oreGold", ModBlocks.nether_gold);
-        OreDictionary.registerOre("oreGold", ModBlocks.end_gold);
-        OreDictionary.registerOre("oreRedstone", ModBlocks.nether_redstone);
-        OreDictionary.registerOre("oreRedstone", ModBlocks.end_redstone);
-        OreDictionary.registerOre("oreDiamond", ModBlocks.nether_diamond);
-        OreDictionary.registerOre("oreDiamond", ModBlocks.end_diamond);
-        OreDictionary.registerOre("oreEmerald", ModBlocks.nether_emerald);
-        OreDictionary.registerOre("oreEmerald", ModBlocks.end_emerald);
-        OreDictionary.registerOre("oreLapis", ModBlocks.nether_lapis);
-        OreDictionary.registerOre("oreLapis", ModBlocks.end_lapis);
+        if (ConfigurationHandler.add_nether_ores && ConfigurationHandler.add_nether_coal)       OreDictionary.registerOre("oreCoal", ModBlocks.nether_coal);
+        if (ConfigurationHandler.add_end_ores && ConfigurationHandler.add_end_coal)             OreDictionary.registerOre("oreCoal", ModBlocks.end_coal);
+        if (ConfigurationHandler.add_nether_ores && ConfigurationHandler.add_nether_iron)       OreDictionary.registerOre("oreIron", ModBlocks.nether_iron);
+        if (ConfigurationHandler.add_end_ores && ConfigurationHandler.add_end_iron)             OreDictionary.registerOre("oreIron", ModBlocks.end_iron);
+        if (ConfigurationHandler.add_nether_ores && ConfigurationHandler.add_nether_gold)       OreDictionary.registerOre("oreGold", ModBlocks.nether_gold);
+        if (ConfigurationHandler.add_end_ores && ConfigurationHandler.add_end_gold)             OreDictionary.registerOre("oreGold", ModBlocks.end_gold);
+        if (ConfigurationHandler.add_nether_ores && ConfigurationHandler.add_nether_redstone)   OreDictionary.registerOre("oreRedstone", ModBlocks.nether_redstone);
+        if (ConfigurationHandler.add_end_ores && ConfigurationHandler.add_end_redstone)         OreDictionary.registerOre("oreRedstone", ModBlocks.end_redstone);
+        if (ConfigurationHandler.add_nether_ores && ConfigurationHandler.add_nether_diamond)    OreDictionary.registerOre("oreDiamond", ModBlocks.nether_diamond);
+        if (ConfigurationHandler.add_end_ores && ConfigurationHandler.add_end_diamond)          OreDictionary.registerOre("oreDiamond", ModBlocks.end_diamond);
+        if (ConfigurationHandler.add_nether_ores && ConfigurationHandler.add_nether_emerald)    OreDictionary.registerOre("oreEmerald", ModBlocks.nether_emerald);
+        if (ConfigurationHandler.add_end_ores && ConfigurationHandler.add_end_emerald)          OreDictionary.registerOre("oreEmerald", ModBlocks.end_emerald);
+        if (ConfigurationHandler.add_nether_ores && ConfigurationHandler.add_nether_lapis)      OreDictionary.registerOre("oreLapis", ModBlocks.nether_lapis);
+        if (ConfigurationHandler.add_end_ores && ConfigurationHandler.add_end_lapis)            OreDictionary.registerOre("oreLapis", ModBlocks.end_lapis);
 
-        OreDictionary.registerOre("oreAluminium", ModBlocks.nether_aluminium);
-        OreDictionary.registerOre("oreAluminium", ModBlocks.end_aluminium);
-        OreDictionary.registerOre("oreAluminium", ModBlocks.overworld_aluminium);
-        OreDictionary.registerOre("oreCobalt", ModBlocks.nether_cobalt);
-        OreDictionary.registerOre("oreCobalt", ModBlocks.end_cobalt);
-        OreDictionary.registerOre("oreCobalt", ModBlocks.overworld_cobalt);
-        OreDictionary.registerOre("oreCopper", ModBlocks.nether_copper);
-        OreDictionary.registerOre("oreCopper", ModBlocks.end_copper);
-        OreDictionary.registerOre("oreCopper", ModBlocks.overworld_copper);
-        OreDictionary.registerOre("oreLead", ModBlocks.nether_lead);
-        OreDictionary.registerOre("oreLead", ModBlocks.end_lead);
-        OreDictionary.registerOre("oreLead", ModBlocks.overworld_lead);
-        OreDictionary.registerOre("oreQuartz", ModBlocks.end_quartz);
-        OreDictionary.registerOre("oreQuartz", ModBlocks.overworld_quartz);
-        OreDictionary.registerOre("oreSilver", ModBlocks.nether_silver);
-        OreDictionary.registerOre("oreSilver", ModBlocks.end_silver);
-        OreDictionary.registerOre("oreSilver", ModBlocks.overworld_silver);
-        OreDictionary.registerOre("oreSulfur", ModBlocks.nether_sulfur);
-        OreDictionary.registerOre("oreSulfur", ModBlocks.end_sulfur);
-        OreDictionary.registerOre("oreSulfur", ModBlocks.overworld_sulfur);
-        OreDictionary.registerOre("oreTin", ModBlocks.nether_tin);
-        OreDictionary.registerOre("oreTin", ModBlocks.end_tin);
-        OreDictionary.registerOre("oreTin", ModBlocks.overworld_tin);
-        OreDictionary.registerOre("oreTitanium", ModBlocks.nether_titanium);
-        OreDictionary.registerOre("oreTitanium", ModBlocks.end_titanium);
-        OreDictionary.registerOre("oreTitanium", ModBlocks.overworld_titanium);
-        OreDictionary.registerOre("oreUranium", ModBlocks.nether_uranium);
-        OreDictionary.registerOre("oreUranium", ModBlocks.end_uranium);
-        OreDictionary.registerOre("oreUranium", ModBlocks.overworld_uranium);
-        OreDictionary.registerOre("oreZinc", ModBlocks.nether_zinc);
-        OreDictionary.registerOre("oreZinc", ModBlocks.end_zinc);
-        OreDictionary.registerOre("oreZinc", ModBlocks.overworld_zinc);
+        if (ConfigurationHandler.add_nether_ores && ConfigurationHandler.add_end_aluminium)             OreDictionary.registerOre("oreAluminium", ModBlocks.nether_aluminium);
+        if (ConfigurationHandler.add_end_ores && ConfigurationHandler.add_nether_aluminium)             OreDictionary.registerOre("oreAluminium", ModBlocks.end_aluminium);
+        if (ConfigurationHandler.add_overworld_ores && ConfigurationHandler.add_overworld_aluminium)    OreDictionary.registerOre("oreAluminium", ModBlocks.overworld_aluminium);
+        if (ConfigurationHandler.add_nether_ores && ConfigurationHandler.add_end_cobalt)                OreDictionary.registerOre("oreCobalt", ModBlocks.nether_cobalt);
+        if (ConfigurationHandler.add_end_ores && ConfigurationHandler.add_nether_cobalt)                OreDictionary.registerOre("oreCobalt", ModBlocks.end_cobalt);
+        if (ConfigurationHandler.add_overworld_ores && ConfigurationHandler.add_overworld_cobalt)       OreDictionary.registerOre("oreCobalt", ModBlocks.overworld_cobalt);
+        if (ConfigurationHandler.add_nether_ores && ConfigurationHandler.add_end_copper)                OreDictionary.registerOre("oreCopper", ModBlocks.nether_copper);
+        if (ConfigurationHandler.add_end_ores && ConfigurationHandler.add_nether_copper)                OreDictionary.registerOre("oreCopper", ModBlocks.end_copper);
+        if (ConfigurationHandler.add_overworld_ores && ConfigurationHandler.add_overworld_copper)       OreDictionary.registerOre("oreCopper", ModBlocks.overworld_copper);
+        if (ConfigurationHandler.add_nether_ores && ConfigurationHandler.add_end_lead)                  OreDictionary.registerOre("oreLead", ModBlocks.nether_lead);
+        if (ConfigurationHandler.add_end_ores && ConfigurationHandler.add_nether_lead)                  OreDictionary.registerOre("oreLead", ModBlocks.end_lead);
+        if (ConfigurationHandler.add_overworld_ores && ConfigurationHandler.add_overworld_lead)         OreDictionary.registerOre("oreLead", ModBlocks.overworld_lead);
+        if (ConfigurationHandler.add_nether_ores && ConfigurationHandler.add_end_quartz)                OreDictionary.registerOre("oreQuartz", ModBlocks.end_quartz);
+        if (ConfigurationHandler.add_end_ores && ConfigurationHandler.add_overworld_quartz)             OreDictionary.registerOre("oreQuartz", ModBlocks.overworld_quartz);
+        if (ConfigurationHandler.add_nether_ores && ConfigurationHandler.add_end_silver)                OreDictionary.registerOre("oreSilver", ModBlocks.nether_silver);
+        if (ConfigurationHandler.add_end_ores && ConfigurationHandler.add_nether_silver)                OreDictionary.registerOre("oreSilver", ModBlocks.end_silver);
+        if (ConfigurationHandler.add_overworld_ores && ConfigurationHandler.add_overworld_silver)       OreDictionary.registerOre("oreSilver", ModBlocks.overworld_silver);
+        if (ConfigurationHandler.add_nether_ores && ConfigurationHandler.add_end_sulfur)                OreDictionary.registerOre("oreSulfur", ModBlocks.nether_sulfur);
+        if (ConfigurationHandler.add_end_ores && ConfigurationHandler.add_nether_sulfur)                OreDictionary.registerOre("oreSulfur", ModBlocks.end_sulfur);
+        if (ConfigurationHandler.add_overworld_ores && ConfigurationHandler.add_overworld_sulfur)       OreDictionary.registerOre("oreSulfur", ModBlocks.overworld_sulfur);
+        if (ConfigurationHandler.add_nether_ores && ConfigurationHandler.add_end_tin)                   OreDictionary.registerOre("oreTin", ModBlocks.nether_tin);
+        if (ConfigurationHandler.add_end_ores && ConfigurationHandler.add_nether_tin)                   OreDictionary.registerOre("oreTin", ModBlocks.end_tin);
+        if (ConfigurationHandler.add_overworld_ores && ConfigurationHandler.add_overworld_tin)          OreDictionary.registerOre("oreTin", ModBlocks.overworld_tin);
+        if (ConfigurationHandler.add_nether_ores && ConfigurationHandler.add_end_titanium)              OreDictionary.registerOre("oreTitanium", ModBlocks.nether_titanium);
+        if (ConfigurationHandler.add_end_ores && ConfigurationHandler.add_nether_titanium)              OreDictionary.registerOre("oreTitanium", ModBlocks.end_titanium);
+        if (ConfigurationHandler.add_overworld_ores && ConfigurationHandler.add_overworld_titanium)     OreDictionary.registerOre("oreTitanium", ModBlocks.overworld_titanium);
+        if (ConfigurationHandler.add_nether_ores && ConfigurationHandler.add_end_uranium)               OreDictionary.registerOre("oreUranium", ModBlocks.nether_uranium);
+        if (ConfigurationHandler.add_end_ores && ConfigurationHandler.add_nether_uranium)               OreDictionary.registerOre("oreUranium", ModBlocks.end_uranium);
+        if (ConfigurationHandler.add_overworld_ores && ConfigurationHandler.add_overworld_uranium)      OreDictionary.registerOre("oreUranium", ModBlocks.overworld_uranium);
+        if (ConfigurationHandler.add_nether_ores && ConfigurationHandler.add_end_zinc)                  OreDictionary.registerOre("oreZinc", ModBlocks.nether_zinc);
+        if (ConfigurationHandler.add_end_ores && ConfigurationHandler.add_nether_zinc)                  OreDictionary.registerOre("oreZinc", ModBlocks.end_zinc);
+        if (ConfigurationHandler.add_overworld_ores && ConfigurationHandler.add_overworld_zinc)         OreDictionary.registerOre("oreZinc", ModBlocks.overworld_zinc);
 
-        OreDictionary.registerOre("ingotAluminium", ModItems.ingot_aluminium);
-        OreDictionary.registerOre("ingotCobalt", ModItems.ingot_cobalt);
-        OreDictionary.registerOre("ingotCopper", ModItems.ingot_copper);
-        OreDictionary.registerOre("ingotLead", ModItems.ingot_lead);
-        OreDictionary.registerOre("ingotSilver", ModItems.ingot_silver);
-        OreDictionary.registerOre("ingotTin", ModItems.ingot_tin);
-        OreDictionary.registerOre("ingotTitanium", ModItems.ingot_titanium);
-        OreDictionary.registerOre("ingotUranium", ModItems.ingot_uranium);
-        OreDictionary.registerOre("ingotZinc", ModItems.ingot_zinc);
+        if ((ConfigurationHandler.add_overworld_ores || ConfigurationHandler.add_nether_ores || ConfigurationHandler.add_end_ores) && (ConfigurationHandler.add_overworld_aluminium || ConfigurationHandler.add_nether_aluminium || ConfigurationHandler.add_end_aluminium)) OreDictionary.registerOre("ingotAluminium", ModItems.ingot_aluminium);
+        if ((ConfigurationHandler.add_overworld_ores || ConfigurationHandler.add_nether_ores || ConfigurationHandler.add_end_ores) && (ConfigurationHandler.add_overworld_cobalt || ConfigurationHandler.add_nether_cobalt || ConfigurationHandler.add_end_cobalt)) OreDictionary.registerOre("ingotCobalt", ModItems.ingot_cobalt);
+        if ((ConfigurationHandler.add_overworld_ores || ConfigurationHandler.add_nether_ores || ConfigurationHandler.add_end_ores) && (ConfigurationHandler.add_overworld_copper || ConfigurationHandler.add_nether_copper || ConfigurationHandler.add_end_copper)) OreDictionary.registerOre("ingotCopper", ModItems.ingot_copper);
+        if ((ConfigurationHandler.add_overworld_ores || ConfigurationHandler.add_nether_ores || ConfigurationHandler.add_end_ores) && (ConfigurationHandler.add_overworld_lead || ConfigurationHandler.add_nether_lead || ConfigurationHandler.add_end_lead)) OreDictionary.registerOre("ingotLead", ModItems.ingot_lead);
+        if ((ConfigurationHandler.add_overworld_ores || ConfigurationHandler.add_nether_ores || ConfigurationHandler.add_end_ores) && (ConfigurationHandler.add_overworld_silver || ConfigurationHandler.add_nether_silver || ConfigurationHandler.add_end_silver)) OreDictionary.registerOre("ingotSilver", ModItems.ingot_silver);
+        if ((ConfigurationHandler.add_overworld_ores || ConfigurationHandler.add_nether_ores || ConfigurationHandler.add_end_ores) && (ConfigurationHandler.add_overworld_tin || ConfigurationHandler.add_nether_tin || ConfigurationHandler.add_end_tin)) OreDictionary.registerOre("ingotTin", ModItems.ingot_tin);
+        if ((ConfigurationHandler.add_overworld_ores || ConfigurationHandler.add_nether_ores || ConfigurationHandler.add_end_ores) && (ConfigurationHandler.add_overworld_titanium || ConfigurationHandler.add_nether_titanium || ConfigurationHandler.add_end_titanium)) OreDictionary.registerOre("ingotTitanium", ModItems.ingot_titanium);
+        if ((ConfigurationHandler.add_overworld_ores || ConfigurationHandler.add_nether_ores || ConfigurationHandler.add_end_ores) && (ConfigurationHandler.add_overworld_uranium || ConfigurationHandler.add_nether_uranium || ConfigurationHandler.add_end_uranium)) OreDictionary.registerOre("ingotUranium", ModItems.ingot_uranium);
+        if ((ConfigurationHandler.add_overworld_ores || ConfigurationHandler.add_nether_ores || ConfigurationHandler.add_end_ores) && (ConfigurationHandler.add_overworld_zinc || ConfigurationHandler.add_nether_zinc || ConfigurationHandler.add_end_zinc)) OreDictionary.registerOre("ingotZinc", ModItems.ingot_zinc);
 
     }
 
--- src/main/java/com/miningmark48/dimensionalores/init/Recipes.java
+++ src/main/java/com/miningmark48/dimensionalores/init/Recipes.java
@@ -9,58 +9,58 @@ public class Recipes {
 
     public static void init(){
         if(ConfigurationHandler.add_nether_ores) {
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_coal), new ItemStack(Items.COAL), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_iron), new ItemStack(Items.IRON_INGOT), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_gold), new ItemStack(Items.GOLD_INGOT), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_diamond), new ItemStack(Items.DIAMOND), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_emerald), new ItemStack(Items.EMERALD), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_redstone), new ItemStack(Items.REDSTONE), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_lapis), new ItemStack(Items.DYE, 1, 4), 1.25f);
+            if (ConfigurationHandler.add_nether_coal) GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_coal), new ItemStack(Items.COAL), 1.25f);
+            if (ConfigurationHandler.add_nether_iron) GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_iron), new ItemStack(Items.IRON_INGOT), 1.25f);
+            if (ConfigurationHandler.add_nether_gold) GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_gold), new ItemStack(Items.GOLD_INGOT), 1.25f);
+            if (ConfigurationHandler.add_nether_diamond) GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_diamond), new ItemStack(Items.DIAMOND), 1.25f);
+            if (ConfigurationHandler.add_nether_emerald) GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_emerald), new ItemStack(Items.EMERALD), 1.25f);
+            if (ConfigurationHandler.add_nether_redstone) GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_redstone), new ItemStack(Items.REDSTONE), 1.25f);
+            if (ConfigurationHandler.add_nether_lapis) GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_lapis), new ItemStack(Items.DYE, 1, 4), 1.25f);
 
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_aluminium), new ItemStack(ModItems.ingot_aluminium), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_cobalt), new ItemStack(ModItems.ingot_cobalt), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_copper), new ItemStack(ModItems.ingot_copper), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_lead), new ItemStack(ModItems.ingot_lead), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_silver), new ItemStack(ModItems.ingot_silver), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_tin), new ItemStack(ModItems.ingot_tin), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_titanium), new ItemStack(ModItems.ingot_titanium), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_uranium), new ItemStack(ModItems.ingot_uranium), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_zinc), new ItemStack(ModItems.ingot_zinc), 1.25f);
+            if (ConfigurationHandler.add_nether_aluminium) GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_aluminium), new ItemStack(ModItems.ingot_aluminium), 1.25f);
+            if (ConfigurationHandler.add_nether_cobalt) GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_cobalt), new ItemStack(ModItems.ingot_cobalt), 1.25f);
+            if (ConfigurationHandler.add_nether_copper) GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_copper), new ItemStack(ModItems.ingot_copper), 1.25f);
+            if (ConfigurationHandler.add_nether_lead) GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_lead), new ItemStack(ModItems.ingot_lead), 1.25f);
+            if (ConfigurationHandler.add_nether_silver) GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_silver), new ItemStack(ModItems.ingot_silver), 1.25f);
+            if (ConfigurationHandler.add_nether_tin) GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_tin), new ItemStack(ModItems.ingot_tin), 1.25f);
+            if (ConfigurationHandler.add_nether_titanium) GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_titanium), new ItemStack(ModItems.ingot_titanium), 1.25f);
+            if (ConfigurationHandler.add_nether_uranium) GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_uranium), new ItemStack(ModItems.ingot_uranium), 1.25f);
+            if (ConfigurationHandler.add_nether_zinc) GameRegistry.addSmelting(new ItemStack(ModBlocks.nether_zinc), new ItemStack(ModItems.ingot_zinc), 1.25f);
         }
 
         if(ConfigurationHandler.add_end_ores) {
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_coal), new ItemStack(Items.COAL), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_iron), new ItemStack(Items.IRON_INGOT), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_gold), new ItemStack(Items.GOLD_INGOT), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_diamond), new ItemStack(Items.DIAMOND), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_emerald), new ItemStack(Items.EMERALD), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_redstone), new ItemStack(Items.REDSTONE), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_lapis), new ItemStack(Items.DYE, 1, 4), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_quartz), new ItemStack(Items.DYE, 1, 4), 1.25f);
+            if (ConfigurationHandler.add_end_coal)  GameRegistry.addSmelting(new ItemStack(ModBlocks.end_coal), new ItemStack(Items.COAL), 1.25f);
+            if (ConfigurationHandler.add_end_iron)  GameRegistry.addSmelting(new ItemStack(ModBlocks.end_iron), new ItemStack(Items.IRON_INGOT), 1.25f);
+            if (ConfigurationHandler.add_end_gold)  GameRegistry.addSmelting(new ItemStack(ModBlocks.end_gold), new ItemStack(Items.GOLD_INGOT), 1.25f);
+            if (ConfigurationHandler.add_end_diamond) GameRegistry.addSmelting(new ItemStack(ModBlocks.end_diamond), new ItemStack(Items.DIAMOND), 1.25f);
+            if (ConfigurationHandler.add_end_emerald) GameRegistry.addSmelting(new ItemStack(ModBlocks.end_emerald), new ItemStack(Items.EMERALD), 1.25f);
+            if (ConfigurationHandler.add_end_redstone) GameRegistry.addSmelting(new ItemStack(ModBlocks.end_redstone), new ItemStack(Items.REDSTONE), 1.25f);
+            if (ConfigurationHandler.add_end_lapis) GameRegistry.addSmelting(new ItemStack(ModBlocks.end_lapis), new ItemStack(Items.DYE, 1, 4), 1.25f);
+            if (ConfigurationHandler.add_end_quartz) GameRegistry.addSmelting(new ItemStack(ModBlocks.end_quartz), new ItemStack(Items.DYE, 1, 4), 1.25f);
 
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_aluminium), new ItemStack(ModItems.ingot_aluminium), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_cobalt), new ItemStack(ModItems.ingot_cobalt), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_copper), new ItemStack(ModItems.ingot_copper), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_lead), new ItemStack(ModItems.ingot_lead), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_silver), new ItemStack(ModItems.ingot_silver), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_tin), new ItemStack(ModItems.ingot_tin), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_titanium), new ItemStack(ModItems.ingot_titanium), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_uranium), new ItemStack(ModItems.ingot_uranium), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.end_zinc), new ItemStack(ModItems.ingot_zinc), 1.25f);
+            if (ConfigurationHandler.add_end_aluminium) GameRegistry.addSmelting(new ItemStack(ModBlocks.end_aluminium), new ItemStack(ModItems.ingot_aluminium), 1.25f);
+            if (ConfigurationHandler.add_end_cobalt) GameRegistry.addSmelting(new ItemStack(ModBlocks.end_cobalt), new ItemStack(ModItems.ingot_cobalt), 1.25f);
+            if (ConfigurationHandler.add_end_copper) GameRegistry.addSmelting(new ItemStack(ModBlocks.end_copper), new ItemStack(ModItems.ingot_copper), 1.25f);
+            if (ConfigurationHandler.add_end_lead) GameRegistry.addSmelting(new ItemStack(ModBlocks.end_lead), new ItemStack(ModItems.ingot_lead), 1.25f);
+            if (ConfigurationHandler.add_end_silver) GameRegistry.addSmelting(new ItemStack(ModBlocks.end_silver), new ItemStack(ModItems.ingot_silver), 1.25f);
+            if (ConfigurationHandler.add_end_tin) GameRegistry.addSmelting(new ItemStack(ModBlocks.end_tin), new ItemStack(ModItems.ingot_tin), 1.25f);
+            if (ConfigurationHandler.add_end_titanium) GameRegistry.addSmelting(new ItemStack(ModBlocks.end_titanium), new ItemStack(ModItems.ingot_titanium), 1.25f);
+            if (ConfigurationHandler.add_end_uranium) GameRegistry.addSmelting(new ItemStack(ModBlocks.end_uranium), new ItemStack(ModItems.ingot_uranium), 1.25f);
+            if (ConfigurationHandler.add_end_zinc) GameRegistry.addSmelting(new ItemStack(ModBlocks.end_zinc), new ItemStack(ModItems.ingot_zinc), 1.25f);
         }
 
         if(ConfigurationHandler.add_overworld_ores){
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_quartz), new ItemStack(Items.QUARTZ), 1.25f);
+            if (ConfigurationHandler.add_overworld_quartz) GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_quartz), new ItemStack(Items.QUARTZ), 1.25f);
 
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_aluminium), new ItemStack(ModItems.ingot_aluminium), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_cobalt), new ItemStack(ModItems.ingot_cobalt), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_copper), new ItemStack(ModItems.ingot_copper), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_lead), new ItemStack(ModItems.ingot_lead), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_silver), new ItemStack(ModItems.ingot_silver), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_tin), new ItemStack(ModItems.ingot_tin), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_titanium), new ItemStack(ModItems.ingot_titanium), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_uranium), new ItemStack(ModItems.ingot_uranium), 1.25f);
-            GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_zinc), new ItemStack(ModItems.ingot_zinc), 1.25f);
+            if (ConfigurationHandler.add_overworld_aluminium) GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_aluminium), new ItemStack(ModItems.ingot_aluminium), 1.25f);
+            if (ConfigurationHandler.add_overworld_cobalt) GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_cobalt), new ItemStack(ModItems.ingot_cobalt), 1.25f);
+            if (ConfigurationHandler.add_overworld_copper) GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_copper), new ItemStack(ModItems.ingot_copper), 1.25f);
+            if (ConfigurationHandler.add_overworld_lead) GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_lead), new ItemStack(ModItems.ingot_lead), 1.25f);
+            if (ConfigurationHandler.add_overworld_silver) GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_silver), new ItemStack(ModItems.ingot_silver), 1.25f);
+            if (ConfigurationHandler.add_overworld_tin) GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_tin), new ItemStack(ModItems.ingot_tin), 1.25f);
+            if (ConfigurationHandler.add_overworld_titanium) GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_titanium), new ItemStack(ModItems.ingot_titanium), 1.25f);
+            if (ConfigurationHandler.add_overworld_uranium) GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_uranium), new ItemStack(ModItems.ingot_uranium), 1.25f);
+            if (ConfigurationHandler.add_overworld_zinc) GameRegistry.addSmelting(new ItemStack(ModBlocks.overworld_zinc), new ItemStack(ModItems.ingot_zinc), 1.25f);
         }
         
     }
