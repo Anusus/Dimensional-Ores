00bf2c31d493c640d62806e2463ba5f86b33a0f9
MiningMark48 <miningmark48@gmail.com>
2017-09-23 20:49:04 -0400

    Updated.

--- src/main/java/com/miningmark48/dimensionalores/reference/Reference.java
+++ src/main/java/com/miningmark48/dimensionalores/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String MOD_ID = "dimensionalores";
     public static final String MOD_NAME = "Dimensional Ores";
-    public static final String VERSION = "1.12-1.0.1";
+    public static final String VERSION = "1.12-1.0.2";
     public static final String CLIENT_PROXY_CLASS = "com.miningmark48.dimensionalores.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.miningmark48.dimensionalores.proxy.CommonProxy";
     public static final String GUI_FACTORY_CLASS = "com.miningmark48.dimensionalores.gui.GuiFactory";
